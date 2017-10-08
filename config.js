27021358f1f68ba7c18f0b52a0b1851eac92595c
Félix Saparelli <felix@passcod.name>
2017-10-09 01:38:50 +1300

    Don't use algolia directly

--- config.js
+++ config.js
@@ -1,9 +1,3 @@
-export const algolia = {
-  app: process.env.ALGOLIA_APP || '2AI1VWJJIN',
-  idx: process.env.ALGOLIA_IDX || 'prod_cogitare',
-  key: process.env.ALGOLIA_KEY
-}
-
 export const dicere = {
   url: (process.env.DICERE_URL || 'https://dicere.cogitare.nz') + '/graphql'
 }
