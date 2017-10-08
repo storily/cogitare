f9e9de341e4e2213fbd2b1e13f962c3c0a3050f6
Félix Saparelli <felix@passcod.name>
2017-10-09 01:43:58 +1300

    Refetch results when query changes

--- views/home/results.js
+++ views/home/results.js
@@ -3,13 +3,15 @@ import { connect } from 'inferno-mobx'
 import { Warning } from '../util/errors'
 import { Item } from '../util/item'
 
+const last = {}
 export default connect(['dicere'], function Results ({ dicere, query }) {
   const { search: { data, error, loading }, fetchSearch } = dicere
 
-  if (!(data || error)) {
+  if (!(data || error) || last.query !== query) {
     fetchSearch({ variables: { query } })
   }
 
+  last.query = query
   return loading
     ? <span>Loading...</span>
     : (error
