776ae773f7454cbb949b4f7be0468fb0a5eea50e
Félix Saparelli <felix@passcod.name>
2017-10-11 18:50:13 +1300

    Fix spurious loading state on search button

--- views/home/index.js
+++ views/home/index.js
@@ -6,17 +6,19 @@ import examples from './examples'
 import Results from './results'
 
 export default connect(['dicere', 'query'], function Home ({ dicere, query }) {
+  const trimmed = query.search.trim()
+  const empty = trimmed.length === 0
+
   return (
     <div className='container home-search'>
       <div className='row justify-content-center'>
         <div className='col-lg-9'>
-          <Search loading={dicere.search.loading} />
+          <Search loading={!empty && dicere.search.loading} />
         </div>
       </div>
-      {query.search.trim().length === 0
-        ? examples()
-        : <Results query={query.search.trim()} />
-      }
+
+      {empty ? examples() : <Results query={trimmed} />}
+
       <Link className='algolia' href='https://algolia.com'>
         <img src='/assets/search-by-algolia.svg' alt='Search by Algolia' width='180' />
       </Link>
