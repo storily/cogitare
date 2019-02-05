99248557b61d840e6fa2d6701be7951d4b9faaf4
Félix Saparelli <felix@passcod.name>
2019-02-06 01:23:48 +1300

    Show two random names alongside prompt

--- views/random/index.js
+++ views/random/index.js
@@ -2,8 +2,20 @@ import Inferno from 'inferno'
 import { connect } from 'inferno-mobx'
 import { Warning } from '../util/errors'
 import { Prompt } from '../util/prompt'
+import { Name } from '../util/name'
 
-export default connect(['dicere'], function Random ({ dicere }) {
+export default function Random () {
+  return (
+    <div className='container home-random'>
+      <div className='row justify-content-center'>
+        <RandomPrompt />
+        <RandomName />
+      </div>
+    </div>
+  )
+}
+
+const RandomPrompt = connect(['dicere'], function RandomPrompt ({ dicere }) {
   const { random: { data, error, loading }, fetchRandom } = dicere
 
   if (!(data || error)) {
@@ -11,21 +23,44 @@ export default connect(['dicere'], function Random ({ dicere }) {
   }
 
   return (
-    <div className='container home-random'>
-      <div className='row justify-content-center'>
-        <div className='col-lg-9'>
-          <button
-            className='btn btn-block btn-outline-success mt-4'
-            type='submit'
-            onClick={fetchRandom}
-            disabled={loading}
-          >
-            {loading ? 'Loading...' : 'Get another'}
-          </button>
+    <div className='col-lg'>
+      <button
+        className='btn btn-block btn-outline-success mt-4'
+        type='submit'
+        onClick={fetchRandom}
+        disabled={loading}
+      >
+        {loading ? 'Loading...' : 'Get another prompt'}
+      </button>
 
-          {error ? <Warning error={error} /> : (data ? <Prompt item={data} /> : null)}
-        </div>
-      </div>
+      {error ? <Warning error={error} /> : (data ? <Prompt item={data} /> : null)}
+    </div>
+  )
+})
+
+const RandomName = connect(['nominare'], function RandomName ({ nominare }) {
+  const { random: { data, error, loading }, fetchRandom } = nominare
+
+  if (!(data || error)) {
+    fetchRandom({ n: 2 })
+  }
+
+  return (
+    <div className='col-lg'>
+      <button
+        className='btn btn-block btn-outline-success mt-4'
+        type='submit'
+        onClick={() => fetchRandom({ n: 2 })}
+        disabled={loading}
+      >
+        {loading ? 'Loading...' : 'Get some other names'}
+      </button>
+
+      {error ? <Warning error={error} /> : (
+        data.length > 0
+          ? data.map(({ first, last }) => <Name first={first} last={last} />)
+          : null
+      )}
     </div>
   )
 })
